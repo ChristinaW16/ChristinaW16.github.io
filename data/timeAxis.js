deccc0cdd5c9ccdfe9af6596afc3c7d495df1204
ChritinaW16 <wangcc_126126@126.com>
2016-11-26 18:04:01 +0800

    resume

---
+++ data/timeAxis.js
@@ -0,0 +1,46 @@
+
+/*		工作时光轴
+	*   {
+	*       startTime: @value String            *         工作开始时间  格式为：2016.10.09
+	*       endTime: @value String 			    *         工作结束时间  格式为：2016.10.09
+	*       companyLogo: @value String 	        *         公司log
+			companyName: @value String          *         公司名称
+	*       companyWebsite: @value String       []        公司网址       格式为：http://www.miaov.com
+	*       jobPost: @value String              *         工作职位
+	*       jonTask: @value String       		*         负责任务
+	*       jobContent: @value String         	*         工作具体内容
+	*
+	*
+	*   }
+*/
+
+var timeAxisArr = [
+		{
+		    startTime:"2015.05",
+			endTime:"2015.11",
+			companyLogo:'images/icon/com_logo1.png',
+			companyNam:"学大教育",
+			companyWebsite:"http://www.xueda.com/",
+			jobPost:"英语教师",
+			jonTask:"教授英语课程",
+			jobContent:"教授英语课程，提高学生的英语听说读写能力。期间接触到了html和css，对其产生浓厚兴趣，开始学习编写静态页面",
+		},
+		{
+		    startTime:"2015.12",
+			endTime:"2016.3",
+			companyLogo:'images/icon/com_logo2.png',
+			companyNam:"嗨学网",
+			companyWebsite:"http://www.haixue.com/",
+			jobPost:"前端开发工程师",
+			jonTask:"编写专题页面",
+			jobContent:"包括：根据UI设计图，专题页静态页面的编写，以及利用jQuery和js完成了一些动态效果，其中包括导航的跳转，倒计时，栏目的显示隐藏，自动轮播，以及滑过变色等效果。",
+		},
+		{
+		    startTime:"2016.04",
+			endTime:"2016.10",
+			companyNam:"妙味课堂",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyWebsite:"http://www.miaov.com",
+			jobContent:"内容包括：从WEB前端开发零基础到、移动端页面的开发、再到原生JAVASCRIPT、Ajax等内容，以上技术比较熟练，还学习了一些类库，如JQuery，还学习到了版本控制工具git和svn、了解到了一些类库和框架如Vue.js、react.js、Node.js等",
+		}
+]
---
+++ data/userInfo.js
@@ -0,0 +1,39 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "王诚诚" , 
+    userPortrait:"images/icon/111.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1139938945",
+    userEmail:   "wcc161016@163.com",
+    motto:  "What hurts more, the pain of hard work or the pain of regret?", 
+    userPhone: 17718530676,
+    userOrignPlace:  "山东省青岛市" ,
+    userSeatPlace:  "北京市",
+    userGithub: "ChristinaW6", 
+    jobWant: "前端工程师",
+//  userExperience: "2年", 
+    userLearning: ["ecmascript 6","react.js","Node.js","Vue.js"],
+    userHobby: ["运动","游戏","电影"],
+    userAssessment: "扎实的JS基础，善于专研新技术，喜欢挑战，有上进心，有比较好的协作和沟通能力，能够流畅阅读英文文档。"
+}
+
